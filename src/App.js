a5e4e1c913f7689611de192b2d6aa2c699069414
josephj <josephj6802@gmail.com>
2017-08-12 02:53:45 +1000

    First working version

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Select from './components/Select/index.js';
+import './components/Select/style.css';
+
+const optionComponent = props => {
+  const style = {
+    display: 'inline-block',
+    marginRight: '10px',
+    width: '10px',
+    height: '10px',
+    borderRadius: '50%',
+    backgroundColor: props.color
+  };
+  return (
+    <span title={props.label}>
+      <span style={style} />
+      {props.label}
+    </span>
+  );
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedValue1: '1',
+      selectedValue2: '2'
+    };
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label>
+          <div>Customised Options</div>
+          <Select
+            value={this.state.selectedValue1}
+            onChange={value => this.setState({ selectedValue1: value })}
+            optionComponent={optionComponent}
+            options={[
+              { value: '1', label: 'Option 1', color: '#aee28a' },
+              { value: '2', label: 'Option 2', color: '#4e92df' },
+              { value: '3', label: 'Option 3', color: '#f3a536' },
+              { value: '4', label: 'Option 4', color: '#fd7173' }
+            ]}
+          />
+        </label>
+
+        <br/><br/>
+
+        <label>
+          <div>Default Options</div>
+          <Select
+            value={this.state.selectedValue2}
+            onChange={value => this.setState({ selectedValue2: value })}
+            options={[
+              { value: '1', label: 'Option 1' },
+              { value: '2', label: 'Option 2' },
+              { value: '3', label: 'Option 3' },
+              { value: '4', label: 'Option 4' }
+            ]}
+          />
+        </label>
+
       </div>
     );
   }
---
+++ src/components/Select/index.js
@@ -0,0 +1,219 @@
+import React, { Component } from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import enhanceWithClickOutside from 'react-click-outside';
+
+class Select extends Component {
+  static displayName = 'Select';
+
+  static propTypes = {
+    className: PropTypes.string,
+    classPrefix: PropTypes.string,
+    isFocused: PropTypes.bool,
+    isMenuOpened: PropTypes.bool,
+    onChange: PropTypes.func,
+    optionComponent: PropTypes.func,
+    options: PropTypes.array.isRequired,
+    value: PropTypes.string
+  };
+
+  static defaultProps = {
+    classPrefix: 'Select',
+    isFocused: false,
+    isMenuOpened: false
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.id = _.uniqueId(`${props.classPrefix}-`);
+
+    this.state = {
+      highlightedValue: -1,
+      isFocused: props.isFocused,
+      isMenuOpened: props.isMenuOpened
+    };
+  }
+
+  handleClickOutside = () => {
+    this.setState({
+      highlightedValue: -1,
+      isMenuOpened: false,
+      isFocused: false
+    });
+  };
+
+  onOptionClick = value => {
+    this.setState({
+      isMenuOpened: false,
+      isFocused: true
+    });
+
+    this.props.onChange(value);
+  };
+
+  onOptionHighlight = value => {
+    this.setState({
+      highlightedValue: value
+    });
+  };
+
+  onSelectionBlur = () => {
+    this.setState({
+      isFocused: false
+    });
+  };
+
+  onSelectionClick = () => {
+    this.setState({
+      highlightedValue: -1,
+      isFocused: true,
+      isMenuOpened: true
+    });
+  };
+
+  onSelectionFocus = () => {
+    this.setState({
+      isFocused: true
+    });
+  };
+
+  onWrapperKeyDown = e => {
+    const { options, onChange, value } = this.props;
+    const { isMenuOpened, highlightedValue } = this.state;
+    let newState = {};
+
+    switch (e.keyCode) {
+      case 9: // Tab
+        if (isMenuOpened) {
+          newState = { isMenuOpened: false, isFocused: false, highlightedValue: -1 };
+        }
+        break;
+      case 13: // Enter
+        if (isMenuOpened) {
+          onChange(highlightedValue);
+          newState = { isMenuOpened: false, isFocused: true, highlightedValue: -1 };
+        } else {
+          newState = { isMenuOpened: true, highlightedValue: value };
+        }
+        break;
+      case 27: // ESC
+        newState = { isMenuOpened: false, isFocused: false };
+        break;
+      case 38: // Up
+        if (isMenuOpened) {
+          const prevIndex = options.map(option => option.value).indexOf(highlightedValue);
+          const nextIndex = prevIndex - 1 >= 0 ? prevIndex - 1 : options.length - 1;
+          newState = { highlightedValue: options[nextIndex].value };
+        } else {
+          console.log(value);
+          newState = { isMenuOpened: true, highlightedValue: value };
+        }
+        break;
+      case 40: // Down
+        if (isMenuOpened) {
+          const prevIndex = options.map(option => option.value).indexOf(highlightedValue);
+          const nextIndex = prevIndex + 1 !== options.length ? prevIndex + 1 : 0;
+          newState = { highlightedValue: options[nextIndex].value };
+        } else {
+          newState = { isMenuOpened: true, highlightedValue: value };
+        }
+        break;
+      default:
+      // ?
+    }
+
+    if (_.size(newState)) {
+      this.setState(newState);
+      e.preventDefault();
+    }
+  };
+
+  renderSelection() {
+    const id = this.id;
+    const { classPrefix, options, optionComponent, value } = this.props;
+    const { highlightedValue, isFocused, isMenuOpened } = this.state;
+    const selectedOption = _.find(options, { value }) || options[0];
+    const ariaProps = {
+      role: 'combobox',
+      'aria-haspopup': true,
+      'aria-expanded': isMenuOpened,
+      'aria-owns': `${id}-results`,
+      'aria-controls': `${id}-results`,
+      'aria-labelledby': `${id}-container`
+    };
+
+    if (isMenuOpened) {
+      ariaProps['aria-activedescendant'] = `${id}-result-${highlightedValue}`;
+    }
+
+    let children;
+    if (optionComponent) {
+      children = optionComponent(selectedOption);
+    } else {
+      children = (
+        <span title={selectedOption.label} id={`${this.id}-container`}>
+          {selectedOption.label}
+        </span>
+      );
+    }
+
+    return (
+      <span
+        tabIndex="0"
+        className={cx(`${classPrefix}-selection`, { 'is-focused': isFocused })}
+        onFocus={this.onSelectionFocus}
+        onBlur={this.onSelectionBlur}
+        onClick={this.onSelectionClick}
+        {...ariaProps}
+        children={children}
+      />
+    );
+  }
+
+  renderOption(option, i) {
+    const { classPrefix, optionComponent, value } = this.props;
+    const { highlightedValue } = this.state;
+    const children = optionComponent ? optionComponent(option) : <span title={option.label}>{option.label}</span>;
+    const className = cx(`${classPrefix}-option`, {
+      'is-highlighted': option.value === highlightedValue,
+      'is-selected': value === option.value
+    });
+
+    return (
+      <li
+        id={`${this.id}-result-${option.value}`}
+        key={`option-${option.value}`}
+        className={className}
+        role="treeitem"
+        aria-selected={value === option.value}
+        onClick={this.onOptionClick.bind(this, option.value)}
+        onMouseOver={this.onOptionHighlight.bind(this, option.value)}
+        children={children}
+      />
+    );
+  }
+
+  render() {
+    const { classPrefix, className, options } = this.props;
+    const { isMenuOpened } = this.state;
+
+    return (
+      <div dir="ltr" className={cx(classPrefix, className)} onKeyDown={this.onWrapperKeyDown}>
+        {this.renderSelection()}
+        <ul
+          id={`${this.id}-results`}
+          role="tree"
+          className={cx(`${classPrefix}-dropdown`, { 'is-hidden': !isMenuOpened })}
+          aria-expanded={isMenuOpened}
+          aria-hidden={!isMenuOpened}
+        >
+          {options.map(this.renderOption.bind(this))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default enhanceWithClickOutside(Select);
